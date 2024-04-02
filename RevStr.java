803f794b611b8913d5ea747a882887698ee64e9c
PATILGAYATRIG <110421842+PATILGAYATRIG@users.noreply.github.com>
2024-04-02 14:34:07 +0530

    Create RevStr.java

---
+++ RevStr.java
@@ -0,0 +1,11 @@
+public class RevStr {
+    public static void main(String[] args) {
+        String[] str ="i like this program very much".split(" ");
+    for(int i=str.length-1;i>=0;i--)
+    {
+      System.out.print(str[i]+" ");
+    }
+        
+    }
+    
+}
