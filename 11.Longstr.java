d8bfbe1bac1c8887d2c5af2fdf686ea0f8e1dda3
PATILGAYATRIG <110421842+PATILGAYATRIG@users.noreply.github.com>
2024-04-07 22:11:34 +0530

    Create 11.Longstr.java

---
+++ 11.Longstr.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+
+public class Longstr
+{
+	public String longestCommonPrefix(String[] a)
+	{
+		int size = a.length;
+
+		
+		if (size == 0)
+			return "";
+
+		if (size == 1)
+			return a[0];
+		Arrays.sort(a);
+
+		
+		int end = Math.min(a[0].length(), a[size-1].length());
+
+	
+		int i = 0;
+		while (i < end && a[0].charAt(i) == a[size-1].charAt(i) )
+			i++;
+
+		String pre = a[0].substring(0, i);
+		return pre;
+	}
+
+
+	public static void main(String[] args)
+	{
+		long str = new long();
+		String[] input = {"geeksforgeeks", "geeks", "geek", "geezer"};
+		System.out.println( "The longest Common Prefix is : " +
+								str.longestCommonPrefix(input));
+	}
+}
